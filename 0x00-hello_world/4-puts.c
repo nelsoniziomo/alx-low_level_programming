34b0a67defaf55e8efaa4d52edbf1895ea31f2bf
“nelsoniziomo” <nelsoniziomo@gmail.com>
2022-09-08 19:17:07 -0700

    Task 4

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,16 @@
+#!/bin/bash                                                                                                                     
+#include <stdio.h>                                                                                                              
+                                                                                                                                
+/**                                                                                                                             
+* main - input the string in the input function                                                                                 
+*                                                                                                                               
+* Description: using the main function                                                                                          
+                                                                                                                                
+* This program prints "Programming is like building a multilingual puzzle                                                       
+* Return: 0                                                                                                                     
+*/                                                                                                                              
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
